1907f05a76defb3d88bfb9b8c0f59a1360dbbc6d
Serafim <serafimabove@mail.ru>
2022-12-06 17:49:06 +0300

    init

--- app.py
+++ app.py
@@ -1,39 +1,44 @@
 import os
+from typing import Iterable, Callable
 
 from constants import BASE_DIR
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from utils import dict_of_utils, log_generator
 
 app = Flask(__name__)
 
 
 @app.post("/perform_query")
-def perform_query():
-    file_name = request.args.get('file_name')
-    cmd1 = request.args.get('cmd1')
-    value1 = request.args.get('value1')
-    cmd2 = request.args.get('cmd2')
-    value2 = request.args.get('value2')
+def perform_query() -> Response:
+    file_name: str = request.args['file_name']
+    cmd1: str = request.args['cmd1']
+    value1: str = request.args['value1']
+    cmd2: str = request.args['cmd2']
+    value2: str = request.args['value2']
 
     if None in (file_name, cmd1, value1, cmd2, value2):
-        return 'Не все поля заполнены', 400
+        return Response('Не все поля заполнены', 400)
 
     if cmd1 not in dict_of_utils or cmd2 not in dict_of_utils:
-        return 'Неизвестная функция', 400
+        return Response('Неизвестная функция', 400)
 
     if not os.path.exists(BASE_DIR + '/data/' + file_name):
-        return 'Файл не найден', 400
+        return Response('Файл не найден', 400)
 
-    default_generator = log_generator()
+    default_generator: Iterable = log_generator()
 
-    first_func = dict_of_utils.get(cmd1)
-    second_func = dict_of_utils.get(cmd2)
+    first_func: Callable = dict_of_utils[cmd1]
+    second_func: Callable = dict_of_utils[cmd2]
 
     first_res = first_func(value1, default_generator)
     second_res = second_func(value2, first_res)
 
-    return jsonify(list(second_res))
+    res_list = list(second_res)
+
+    return jsonify(res_list)
 
 
 if __name__ == '__main__':
     app.run()
+
+
--- utils.py
+++ utils.py
@@ -1,45 +1,48 @@
+import re
+from typing import Generator, Iterable, Pattern, Dict, Callable, Optional
+
 from constants import DATA_DIR
 
 
-def log_generator():
+def log_generator() -> Iterable:
     with open(DATA_DIR) as file:
         log_sting = file.readlines()
         for log in log_sting:
             yield log
 
 
-def user_filter(value, generator):
+def user_filter(value: str, generator: Iterable) -> Iterable:
     return filter(lambda x: value in x, generator)
 
 
-def user_map(num, generator):
+def user_map(num: str, generator: Iterable) -> Iterable:
     return map(lambda string: string.split()[int(num)], generator)
 
 
-def user_unique(value, generator):
-    listt = []
+def user_unique(value, generator: Iterable) -> Iterable:
+    listt: list[str] = []
     for string in generator:
         if string not in listt:
             listt.append(string)
             yield string
 
 
-def user_sort(order, generator):
+def user_sort(order: str, generator: Iterable) -> Iterable:
     reverse = None
 
     if order == 'asc':
         reverse = False
-    elif order == 'desc':
+    else:
         reverse = True
 
     for string in sorted(generator, reverse=reverse):
         yield string
 
 
-def user_limit(num, generator):
-    counter = 1
+def user_limit(num: str, generator: Iterable) -> Iterable:
+    counter: int = 1
     for string in generator:
-        if counter > num:
+        if counter > int(num):
             break
 
         counter += 1
@@ -47,10 +50,18 @@ def user_limit(num, generator):
         yield string
 
 
-dict_of_utils = {
+def regex(value: str, generator: Iterable) -> Iterable:
+    pattern: Pattern = re.compile(value)
+    for string in generator:
+        if re.search(pattern, string):
+            yield string
+
+
+dict_of_utils: Dict[str, Callable] = {
     'filter': user_filter,
     'map': user_map,
     'unique': user_unique,
     'sort': user_sort,
-    'limit': user_limit
+    'limit': user_limit,
+    'regex': regex
 }
